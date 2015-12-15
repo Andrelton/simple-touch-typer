fbde9b3a61a709336bcb4879b13ce26328e38da9
Andrelton <eric.saxman@gmail.com>
2015-12-15 02:21:46 -0800

    add sloppy proof of concept for moving cursor and testing keyCode

--- index.js
+++ index.js
@@ -33,17 +33,38 @@ App.doThing = function() {
 
 App.keyListen = function() {
   $(window).on('keydown', function(event) {
-    event.preventDefault;
+    event.preventDefault();
 
-    console.log(event.keyCode);
+    var currentTestLetter = $('h1.text-string span.current').text();
+    var currentTestLetterCode = currentTestLetter.charCodeAt(0);
+
+    if (currentTestLetterCode === event.keyCode) {
+      $("h1.title").addClass('wild');
+      setTimeout(function() {
+        $('h1.title').removeClass('wild');
+      }, 500);
+    }
+    App.moveCursor();
   })
+}
+
+App.moveCursor = function(){
+  var futureText = $('h1.text-string span.future').text();
+  var currentText = $('h1.text-string span.current').text();
+  var pastText = $('h1.text-string span.past').text();
+
+  $('h1.text-string span.past').text(pastText + currentText);
+  $('h1.text-string span.current').text(futureText.slice(0, 1));
+  $('h1.text-string span.future').text(futureText.slice(1, -1));
 }
 
 App.init = function() {
   setTimeout(function() {
     $('h1.title').addClass('blaze');
-    $('h1.text-string .future').text(App.doThing());
+    $('h1.text-string span.future').text(App.doThing());
+    App.moveCursor();
   }, 500);
+
 };
 
 $(function() {
